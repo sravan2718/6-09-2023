ba332fd5c5926de743cdb957cb92cb76bcd68c2b
sravan2718 <sravan.d@thundersoft.com>
2023-09-06 13:11:52 -0700

    my 1st cprograms

---
+++ firstprogram.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main(){
+    int myfavoritenumber = 0;
+    printf("please enter your number:\n");
+    scanf("%d",&myfavoritenumber);
+    printf("your entered number :%d\n",myfavoritenumber);
+    
+    return 0;
+    
+}
